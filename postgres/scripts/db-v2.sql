9693e602c72eca23852b1c657d63c28bf9fd25c6
sergueibaskakov <sergueibaskakov@gmail.com>
2022-11-21 02:01:10 +0300

    4

--- postgres/scripts/db-v2.sql
+++ postgres/scripts/db-v2.sql
@@ -1,52 +1,8 @@
 CREATE DATABASE payments;
 GRANT ALL PRIVILEGES ON DATABASE payments TO program;
 
-CREATE TABLE payments.payment
-(
-    id          SERIAL PRIMARY KEY,
-    payment_uid uuid        NOT NULL,
-    status      VARCHAR(20) NOT NULL
-        CHECK (status IN ('PAID', 'CANCELED')),
-    price       INT         NOT NULL
-);
-
 CREATE DATABASE reservations;
 GRANT ALL PRIVILEGES ON DATABASE reservations TO program;
 
-CREATE TABLE reservations.reservation
-(
-    id              SERIAL PRIMARY KEY,
-    reservation_uid uuid UNIQUE NOT NULL,
-    username        VARCHAR(80) NOT NULL,
-    payment_uid     uuid        NOT NULL,
-    hotel_id        INT REFERENCES hotels (id),
-    status          VARCHAR(20) NOT NULL
-        CHECK (status IN ('PAID', 'CANCELED')),
-    start_date      TIMESTAMP WITH TIME ZONE,
-    end_data        TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE reservations.hotels
-(
-    id        SERIAL PRIMARY KEY,
-    hotel_uid uuid         NOT NULL UNIQUE,
-    name      VARCHAR(255) NOT NULL,
-    country   VARCHAR(80)  NOT NULL,
-    city      VARCHAR(80)  NOT NULL,
-    address   VARCHAR(255) NOT NULL,
-    stars     INT,
-    price     INT          NOT NULL
-);
-
 CREATE DATABASE loyalties;
 GRANT ALL PRIVILEGES ON DATABASE loyalties TO program;
-
-CREATE TABLE loyalties.loyalty
-(
-    id                SERIAL PRIMARY KEY,
-    username          VARCHAR(80) NOT NULL UNIQUE,
-    reservation_count INT         NOT NULL DEFAULT 0,
-    status            VARCHAR(80) NOT NULL DEFAULT 'BRONZE'
-        CHECK (status IN ('BRONZE', 'SILVER', 'GOLD')),
-    discount          INT         NOT NULL
-);
